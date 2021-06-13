de10309b6eefae39cb1456d9adb8ae6bd3f74262
Mulindi <Djangowebapp@gmail.com>
2021-06-13 14:47:11 +0300

    Added instructions on how the application works.

--- app/src/main/java/com/example/smartrecipe/ui/howItWorks/HowItWorks.kt
+++ app/src/main/java/com/example/smartrecipe/ui/howItWorks/HowItWorks.kt
@@ -14,35 +14,15 @@ import com.example.smartrecipe.ui.categories.CategoriesViewModel
 
 class HowItWorks : Fragment() {
 
-    private lateinit var viewModel: HowItWorksViewModel
-    private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? { viewModel =
-        ViewModelProvider(this).get(HowItWorksViewModel::class.java)
-
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        viewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+    ): View? {
+        return inflater.inflate(R.layout.how_it_works_fragment, container, false)
 
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
+
 
 }
