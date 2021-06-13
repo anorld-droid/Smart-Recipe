a830a28562ba39eed1284c95a46ab3f589eef82c
Mulindi <Djangowebapp@gmail.com>
2021-06-13 19:51:24 +0300

    Added an image as the background of fragment_home.xml with two text view on top.

--- app/src/main/java/com/example/smartrecipe/ui/categories/Categories.kt
+++ app/src/main/java/com/example/smartrecipe/ui/categories/Categories.kt
@@ -1,5 +1,7 @@
 package com.example.smartrecipe.ui.categories
 
+import android.content.Intent
+import android.net.Uri
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -9,27 +11,28 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.lifecycle.Observer
 import com.example.smartrecipe.R
+import com.example.smartrecipe.databinding.FragmentCategoriesBinding
 import com.example.smartrecipe.databinding.FragmentHomeBinding
-import com.example.smartrecipe.ui.home.HomeViewModel
+import com.example.smartrecipe.ui.membership.MembershipViewModel
+import com.google.android.material.chip.Chip
 
 class Categories : Fragment() {
-
-    private lateinit var viewModel: CategoriesViewModel
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentCategoriesBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
 
+    private lateinit var viewModel: CategoriesViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-       viewModel =
+        viewModel =
             ViewModelProvider(this).get(CategoriesViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentCategoriesBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
@@ -37,12 +40,5 @@ class Categories : Fragment() {
             textView.text = it
         })
         return root
-
     }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-
 }
--- app/src/main/java/com/example/smartrecipe/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/smartrecipe/ui/home/HomeFragment.kt
@@ -13,44 +13,14 @@ import com.example.smartrecipe.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
-    private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-//        setHasOptionsMenu(true)
-         homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        return inflater.inflate(R.layout.about_fragment, container, false)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
     }
-//    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-//        super.onCreateOptionsMenu(menu, inflater)
-//        inflater?.inflate(R.menu.main, menu)
-//    }
-//
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        return NavigationUI.onNavDestinationSelected(item!!,
-//            requireView().findNavController()) || super.onOptionsItemSelected(item)
-//    }
-//
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
 }
