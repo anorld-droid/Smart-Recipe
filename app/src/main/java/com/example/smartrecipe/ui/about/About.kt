ab7e98630b06b824c36253b0ac9206cdf8e4564a
Mulindi <Djangowebapp@gmail.com>
2021-06-13 12:29:53 +0300

    Added next line characters in about text strings.xml.

--- app/src/main/java/com/example/smartrecipe/ui/about/About.kt
+++ app/src/main/java/com/example/smartrecipe/ui/about/About.kt
@@ -13,41 +13,12 @@ import com.example.smartrecipe.R
 import com.example.smartrecipe.databinding.FragmentHomeBinding
 import com.example.smartrecipe.ui.categories.CategoriesViewModel
 
-class About : Fragment() {
-
-    private lateinit var viewModel: AboutViewModel
-    private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
+class About : Fragment(){
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        viewModel =
-            ViewModelProvider(this).get(AboutViewModel::class.java)
+        return inflater.inflate(R.layout.about_fragment, container, false)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        viewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-
-        return root
     }
-
-//    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-//        super.onCreateOptionsMenu(menu, inflater)
-//        inflater?.inflate(R.menu.main, menu)
-//    }
-
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        return NavigationUI.onNavDestinationSelected(item!!,
-//            requireView().findNavController()) || super.onOptionsItemSelected(item)
-//    }
-
 }
